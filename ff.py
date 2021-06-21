a2b4ba138a99d269076d815344b5e5467483fbdb
drinz <86057200+drinz@users.noreply.github.com>
2021-06-21 20:40:18 +0700

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,28 @@
+#code hackff
+
+#hackdylandpros
+
+import random
+import string
+import time
+
+print("--> tools hack free fire by rizki farez😎 <--")
+print("#-----------#")
+print("#---hello---#")
+print("#___________#")
+
+ip = str(input(" username ff:"))
+port = str(input(" id:"))
+tunggu = str(input("tunggu 10 detik kami akan mencek akun anda(y)"))
+
+time.sleep(10)
+
+asu = str(input("apakah akun bernama %s akun yang anda ingin hack?:"%(ip)))
+time.sleep(10)
+hahay = str(input("apakah anda ingin menghack akun bernama %s:"%(ip)))
+
+
+time.sleep(10)
+print("akun bernama %s berhasil dihack"%(ip))
+aku = str(input("apakah anda ingin melihat password akun %s?(y):"%(ip)))
+print("password akunya adalah goblok mau aja ditipu aowkkwkw")
